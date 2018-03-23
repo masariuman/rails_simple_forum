2a86725c9c9d14faa844fdd663394833444c280e
masariuman <masariuman@gmail.com>
2018-03-23 18:58:13 +0700

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "forum_threada#index"
+  root "forum_threads#index"
 end
