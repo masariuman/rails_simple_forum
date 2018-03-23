262ee37359d52a2d70e5a10436cae1caa57afc7d
masariuman <masariuman@gmail.com>
2018-03-23 21:05:57 +0700

    db/migrate/20180323140212_add_forum_posts_counter_to_forum_threads.rb

--- db/migrate/20180323140212_add_forum_posts_counter_to_forum_threads.rb
+++ db/migrate/20180323140212_add_forum_posts_counter_to_forum_threads.rb
@@ -1,5 +1,13 @@
 class AddForumPostsCounterToForumThreads < ActiveRecord::Migration[5.1]
-  def change
-    add_column :forum_threads, :forum_posts_counter, :integer, default: 0
+  def up
+    add_column :forum_threads, :forum_posts_count, :integer, default: 0
+
+    ForumThread.all.each do |as|
+      ForumThread.reset_counters(as.id, :forum_posts)
+    end
+  end
+
+  def down
+    remove_column :forum_threads, :forum_posts_count
   end
 end
